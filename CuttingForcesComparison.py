c02665be375c97e265a372e88c290470b1611880
rafaelborgesleal <rafaelborgesleal@hotmail.com>
2025-10-09 22:32:36 +0000

    Save

--- CuttingForcesComparison.py
+++ CuttingForcesComparison.py
@@ -22,3 +22,27 @@ def phi_leeshaffer(alpha_deg, mu):
 phis = phi_merchant(alphas_deg, 0.3)
 print(phis)
 
+# Step 03: Define the function to solve cutting and thrust forces
+
+def solve_forces(alpha_deg, phi_deg, mu, tau_s, t0, b):
+    alpha = np.radians(alpha_deg)
+    phi = np.radians(phi_deg)
+
+    As = b * t0 / np.sin(phi)
+
+    Fs = tau_s * As
+
+    A = np.array([
+        [np.cos(phi), -np.sin(phi)],
+        [np.sin(alpha) - mu * np.cos(alpha),np.cos(alpha) + mu * np.sin(alpha)]
+    ])
+
+    rhs = np.array([Fs, 0.0])
+
+    Fc, Ft = np.linalg.solve(A, rhs)
+    return Fc, Ft, Fs, As
+
+phi = phi_merchant(10, 0.3)
+Fc, Ft, Fs, As = solve_forces(10, phi, 0.3, tau_s, t0, b)
+print(Fc, Ft, Fs, As)
+
